44cef125378d5fa06426ec7dc19d9623960314d7
ERIC-ZHAO-3366 <3177261635@qq.com>
2025-10-01 15:29:57 +0800

    change sth.

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 // src/config.ts
 // 针对博主特定的数据
 export const ADMIN_USER = {
-  nickname: "EveSunMaple",
-  email: "evesunmaple@outlook.com",
-  website: "https://www.saroprock.com",
-  avatar: "https://www.saroprock.com/avatar.webp",
+  nickname: "EricZhao",
+  email: "ericzhao3366@dingtalk.com",
+  website: "https://blog.ericzhao3366.work",
+  avatar: "./avatar.webp",
 };
 
 // 你不希望用户使用的名称与邮箱
-export const SENSITIVE_USERS = ["evesunmaple", "EveSunMaple", "sunmaple", "SunMaple", "admin", "博主", "evesunmaple@outlook.com"];
+export const SENSITIVE_USERS = ["ericzhao", "ericzhao3366", "EricZhao3366", "ERICZHAO","EricZhao", "admin", "博主", "ericzhao3366@dingtalk.com"];
